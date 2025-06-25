940fa5ce4f45dc3dec87c122bfc6041bd05eed38
akmlrafly <akmalrafly875@gmail.com>
2025-06-26 05:51:45 +0700

    fix: promo-section

--- src/components/molecules/card/card-indicator.tsx
+++ src/components/molecules/card/card-indicator.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 function CardIndicator({ icon, title, subtitle }: CategoriesProps) {
   return (
-    <div className="relative w-full h-[200px] rounded-2xl overflow-hidden shadow-md">
+    <div className="relative w-full h-80 rounded-2xl overflow-hidden shadow-md">
       {/* Background image */}
       <Image src={icon} alt={title} fill className="object-cover" />
 
-      {/* Absolute Text Top Right */}
+      {/* Logo (Top Right) */}
       <div className="absolute top-2 right-2 bg-white/30 backdrop-blur-md rounded-lg px-2 flex items-center gap-2">
         <Image
           src="/logo-beauty.png"
@@ -16,6 +16,12 @@ function CardIndicator({ icon, title, subtitle }: CategoriesProps) {
           width={100}
           className="w-[80px] md:w-[100px]"
         />
+      </div>
+
+      {/* Title & Subtitle (Bottom, with masking) */}
+      <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/60 via-black/30 to-transparent text-white p-4">
+        <h3 className="text-lg font-semibold">{title}</h3>
+        <p className="text-sm">{subtitle}</p>
       </div>
     </div>
   );
